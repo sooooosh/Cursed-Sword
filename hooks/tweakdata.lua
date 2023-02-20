ac621bd32c0f231793832660a4a434909601652c
soosh.exe <37846872+sooooosh@users.noreply.github.com>
2023-02-20 17:38:52 +0100

    backup
    - temporary objective scripting
    - added pc instance (work in progress)
    - started decorating server, storage and office rooms
    - added vent gas with killzone (untested)

--- hooks/tweakdata.lua
+++ hooks/tweakdata.lua
@@ -1,3 +1,3 @@
 	tweak_data.pickups.blade_phone = {
-		unit = Idstring("units/payday2/pickups/gen_pku_crowbar/gen_pku_crowbar")
+		unit = Idstring("units/pd2_mod_blade/equipment/blade_iphone/blade_iphone")
 	}
